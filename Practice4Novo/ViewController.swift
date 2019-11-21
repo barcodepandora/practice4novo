a826ab437416fe1c4e57fb2ed2bd93ad2d372b70
Juan Manuel Moreno <barcodepandora@gmail.com>
2019-11-21 09:25:49 -0500

    First

---
+++ Practice4Novo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice4Novo
+//
+//  Created by Juan Manuel Moreno on 21/11/2019.
+//  Copyright © 2019 Juan Manuel Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
